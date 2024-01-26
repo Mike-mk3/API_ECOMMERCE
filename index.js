4470ff5bd3a414123a3428ab900d61a65a578a6a
Mike-mk3 <mike666mtz@gmail.com>
2024-01-25 23:16:20 -0600

    agregado de rutas
    get-post-put-delete

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const puerto = 3000;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, Express!');
+});
+
+
+
+
+
+app.get('/products', (req, res) => {
+    res.send('GET /products');
+});
+
+app.post('/products', (req, res) => {
+    res.send('POST /products');
+});
+
+app.put('/products', (req, res) => {
+    res.send('PUT /products');
+});
+
+app.delete('/products', (req, res) => {
+    res.send('DELETE /products');
+});
+
+
+
+
+
+
+app.listen(puerto, () => {
+    console.log('Servidor escuchando en http://localhost:' + puerto);
+});
